43600806a6b002aad800c8bdde30a2f1d80d2940
Ole Hansen <ole@jlab.org>
2007-08-14 03:17:32 +0000

    Add copy ctor and operator= to Bits. Add FastClear(), which can be inlined easily unlike virtual Clear(). Add getters to Hitpattern. Implement skeleton SetPoints.

--- Hitpattern.cxx
+++ Hitpattern.cxx
@@ -7,6 +7,7 @@
 #include "Hitpattern.h"
 #include "Hit.h"
 #include "TSeqCollection.h"
+#include "MWDC.h"
 
 #include <iostream>
 
@@ -23,7 +24,7 @@ Hitpattern::Hitpattern( UInt_t depth, UInt_t nplanes, Double_t width )
 {
   // Constructor
 
-  if( fNplanes > 0 && fDepth > 0 ) {
+  if( fNplanes > 0 ) {
     fPattern = new Bits*[fNplanes];
     for( UInt_t i=0; i<fNplanes; i++ )
       fPattern[i] = new Bits( 1U<<fDepth );
@@ -40,22 +41,7 @@ Hitpattern::~Hitpattern()
 
   for( UInt_t i=0; i<fNplanes; i++ )
     delete fPattern[i];
-  delete fPattern;
-}
-
-//_____________________________________________________________________________
-Int_t Hitpattern::SetPoints( TSeqCollection* hitsA, TSeqCollection* hitsB, 
-			     UInt_t plane )
-{
-  // Set all bits at all depths of the hit pattern corresponding to
-  // the hits in 'hitsA' in plane number 'plane'.
-  // The second collection, 'hitsB', represents hits in a optional partner
-  // plane of the 'hitsA' plane, usually a nearby second plane with staggered
-  // wires. Naturally, the two planes and their wires must be parallel.
-
-  return 0;
-
-
+  delete [] fPattern;
 }
 
 //_____________________________________________________________________________
@@ -64,7 +50,45 @@ void Hitpattern::Clear( Option_t* opt )
   // Clear the hitpattern
 
   for( UInt_t i=0; i<fNplanes; i++ )
-    fPattern[i]->Clear();
+    fPattern[i]->FastClear();
+}
+
+//_____________________________________________________________________________
+Int_t Hitpattern::SetPoints( WirePlane* A, WirePlane* B, UInt_t plane )
+{
+  // Set all bits at all depths of the hit pattern corresponding to
+  // the hits in 'A' in plane number 'plane'.
+  // The second, optional plane 'B' represents an optional partner
+  // plane of 'A', usually a nearby plane with staggered wires. 
+  // Naturally, the two planes and their wires must be parallel.
+  //
+  // Returns number of hits processed
+
+  static const ObjPair_t null(0,0);
+  
+  Clear();
+
+  if( !A )
+    return 0;
+  MWDC* mwdc = dynamic_cast<MWDC*>( A->GetDetector() );
+  if( !mwdc )
+    return 0;
+  Double_t dz = B ? B->GetZ() - A->GetZ() : 0.0;
+  Double_t maxslope = mwdc->GetMaxSlope();
+  Double_t maxdist = maxslope * dz;
+
+  Int_t nhits = 0;
+
+  HitPairIter next( A->GetHits(), B ? B->GetHits() : NULL, maxdist );
+  ObjPair_t& hitpair = next();
+  while( hitpair != null ) {
+    nhits++;
+    
+    hitpair = next();
+  }
+
+
+  return nhits;
 }
 
 //_____________________________________________________________________________
--- Hitpattern.h
+++ Hitpattern.h
@@ -7,12 +7,8 @@
 //                                                                           //
 ///////////////////////////////////////////////////////////////////////////////
 
-//FIXME: add Draw() (=event display)
-
 #include "TBits.h"
 #include <cstring>
-
-class TSeqCollection;
 
 namespace TreeSearch {
 
@@ -20,15 +16,19 @@ namespace TreeSearch {
   // reserved memory
   class Bits : public TBits {
   public:
-    Bits( UInt_t nbits ) : TBits(nbits) {}
-    virtual ~Bits() {}
-    virtual void Clear( Option_t* opt="" ) { 
-      if (fAllBits) memset(fAllBits,0,fNbytes);
+    Bits( UInt_t nbits = 8 ) : TBits(nbits) {}
+    Bits( const TBits& orig ) : TBits(orig) {}
+    Bits& operator=( const TBits& rhs ) {
+      TBits::operator=(rhs); return *this;
     }
+    virtual ~Bits() {}
+    void FastClear() { memset(fAllBits,0,fNbytes); }
+    virtual void Clear( Option_t* opt="" ) { if (fAllBits) FastClear(); }
     ClassDef(Bits,1)  // Bit container with memory-neutral Clear method
   };
 
   class PatternTree;
+  class WirePlane;
 
   class Hitpattern {
 
@@ -37,11 +37,17 @@ namespace TreeSearch {
     Hitpattern( UInt_t depth, UInt_t nplanes, Double_t width );
     virtual ~Hitpattern();
 
-    Int_t SetPoints( TSeqCollection* hitsA, TSeqCollection* hitsB,
-		     UInt_t plane );
+    Bits*  GetRow( UInt_t i ) const { return i<fNplanes ? fPattern[i] : 0; }
+    Bits** GetPattern() const { return fPattern; }
+    Double_t GetWidth() const { return fWidth; }
 
-    void Clear( Option_t* opt="" );
-    void Print( Option_t* opt="" ) const;
+    Int_t  SetPoints( WirePlane* hitsA, WirePlane* hitsB,
+		      UInt_t plane );
+
+    void   Clear( Option_t* opt="" );
+    void   Print( Option_t* opt="" ) const;
+
+//FIXME: add Draw() (=event display)
 
   protected:
 
