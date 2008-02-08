760af5c6da3617dc0c3c87df960b7eab4fdaa7ac
Ole Hansen <ole@jlab.org>
2008-02-08 16:17:09 +0000

    Add first set of data members. Work in progress.

--- Road.h
+++ Road.h
@@ -8,26 +8,51 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 #include "Rtypes.h"
+#include <vector>
+
+using std::vector;
 
 namespace TreeSearch {
 
-  class NodeDescriptor;
+  class NodeDescriptor; // Defined in TreeWalk.h
+  class BuildInfo_t;    // Defined in implementation
 
   class Road {
 
   public:
-    Road( NodeDescriptor* nd ) {}
+    explicit Road( NodeDescriptor* nd ) {}
     Road( const Road& ) {}
     Road& operator=( const Road& );
     virtual ~Road() {}
 
-    Int_t Add( NodeDescriptor* nd ) { return 0;}
-    Int_t Close() { return 0; }
-    UInt_t GetNpat() const { return 0; };
+    Bool_t Add( NodeDescriptor* nd );
+    void   Close();
 
     void Print( Option_t* opt="" ) const;
 
   protected:
+
+    // Bin numbers defining the corners
+    UShort_t  fStart[2];
+    UShort_t  fEnd[2];
+
+    // Fit results
+    Double_t  fSlope;
+    Double_t  fPos;
+    Double_t  fChi2;
+    Double_t  fErr[2];
+
+    // Data used while building
+    BuildInfo_t* fBifo;
+    //TODO:
+    // array of hit positions within the road (x/z coordinates)
+    // function to collect these
+    // array of NodeDescriptors* collected
+    // array of common hits
+    // array of counters of common hits per plane
+    // match evaluation function (deals with missing planes, 
+    //     1 out of 2 handling etc)
+    
 
     ClassDef(Road,1)  // Region containing track candidate hits 
   };
