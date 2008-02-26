b74b3f3abfdd0b034a98b2b87bb336d7b4e88677
Ole Hansen <ole@jlab.org>
2008-02-26 02:12:49 +0000

    Add UniqueCombo iterator over unqiue combinations of k numbers out of N.

--- Helper.h
+++ Helper.h
@@ -52,11 +52,52 @@ namespace TreeSearch {
 				 std::vector<VectorElem>, 
 				 typename std::vector<VectorElem>::size_type >
   {
-    // Get the product of the sizes of the vectors in a vector, ignoring 
-    // empty ones
+    // Function object to get the product of the sizes of the vectors in a 
+    // vector, ignoring empty ones
     typedef typename std::vector<VectorElem>::size_type vsiz_t;
     vsiz_t operator() ( vsiz_t val, const std::vector<VectorElem>& vec ) const 
     { return ( vec.empty() ? val : val * vec.size() ); }
+  };
+
+  //___________________________________________________________________________
+  // Iterator over unique combinations of k out of N elements. The element 
+  // numbers are accessible in a vector<int> of size k.
+  class UniqueCombo {
+    typedef std::vector<int> vint_t;
+  public:
+    UniqueCombo( int N, int k ) : fN(N), fGood(true)
+    { assert( 0<k && k<=N ); for( int i=0; i<k; ++i ) fCurrent.push_back(i); }
+    // Default copy and assignment are fine
+
+    UniqueCombo& operator++()
+    { fGood = recursive_plus( fCurrent.size()-1 ); return *this; }
+    const UniqueCombo operator++(int)
+    { UniqueCombo clone(*this); ++*this; return clone; }
+    const vint_t& operator()() const { return fCurrent; }
+    const vint_t& operator*()  const { return fCurrent; }
+    bool operator==( const UniqueCombo& rhs ) const
+    { return ( fN == rhs.fN and fCurrent == rhs.fCurrent ); }
+    bool operator!=( const UniqueCombo& rhs ) const { return !(*this==rhs); }
+    operator bool() const  { return fGood; }
+    bool operator!() const { return !((bool)*this); }
+    
+  private:
+    bool recursive_plus( vint_t::size_type pos ) {
+      if( fCurrent[pos] < fN+int(pos-fCurrent.size()) ) {
+	++fCurrent[pos];
+	return true;
+      }
+      if( pos==0 )
+	return false;
+      if( recursive_plus(pos-1) ) {
+	fCurrent[pos] = fCurrent[pos-1]+1;
+	return true;
+      }
+      return false;
+    }
+    int    fN;
+    vint_t fCurrent;
+    bool   fGood;
   };
 
 ///////////////////////////////////////////////////////////////////////////////
