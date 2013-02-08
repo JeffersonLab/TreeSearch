98bc2b6db86ccbabad29cb3c9d0d7394eea67077
Ole Hansen <ole@jlab.org>
2013-02-08 16:29:47 -0500

    First version of the generic Plane class.

---
+++ Plane.cxx
@@ -0,0 +1,327 @@
+//*-- Author :    Ole Hansen, Jefferson Lab   11-Jan-2010
+
+//////////////////////////////////////////////////////////////////////////////
+//                                                                          //
+// TreeSearch::Plane                                                        //
+//                                                                          //
+// A 1-dimensional readout plane of a GEM chamber.                          //
+// Use two of these objects for a 2-d readout plane.                        //
+//                                                                          //
+//////////////////////////////////////////////////////////////////////////////
+
+#include "Plane.h"
+#include "Hit.h"
+#include "Tracker.h"
+#include "Projection.h"
+
+#include "THaDetMap.h"
+#include "THaEvData.h"
+#include "TClonesArray.h"
+#include "TH1.h"
+
+#include "TClass.h"
+
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+using namespace std;
+
+namespace TreeSearch {
+
+//_____________________________________________________________________________
+Plane::Plane( const char* name, const char* description,
+	      THaDetectorBase* parent )
+  : THaSubDetector(name,description,parent), fPlaneNum(kMaxUInt),
+    fDefinedNum(kMaxUInt), fType(kUndefinedType), fStart(0), 
+    fPitch(0), fCoordOffset(0), fPartner(0), fProjection(0),
+    fResolution(0), fMaxHits(kMaxUInt), fHits(0), fFitCoords(0),
+    fHitMap(0)
+{
+  // Constructor
+
+  assert( name && parent );
+
+#ifdef TESTCODE
+  // No diagnostic histograms by default
+  SetBit(kDoHistos,0);
+#endif
+}
+
+//_____________________________________________________________________________
+Plane::~Plane()
+{
+  // Destructor.
+
+  // Histograms in fHist should be automatically deleted by ROOT when 
+  // the output file is closed
+
+  if( fIsSetup )
+    RemoveVariables();
+
+  delete fFitCoords;
+  delete fHits;
+}
+
+//_____________________________________________________________________________
+FitCoord* Plane::AddFitCoord( const FitCoord& coord )
+{ 
+  // Add given fit coordinate data to this plane's array of fit coordinates
+
+  return
+    new( (*fFitCoords)[GetNcoords()] ) FitCoord(coord);
+}
+
+//_____________________________________________________________________________
+Int_t Plane::Begin( THaRunBase* /* run */ )
+{
+#ifdef TESTCODE
+  // Book diagnostic histos if not already done
+
+  if( TestBit(kDoHistos) ) {
+    //TODO: check if exist
+    string hname, htitle;
+    hname   = fName + "_hitmap";
+    htitle  = fName + " hitmap";
+    fHitMap = new TH1F( hname.c_str(), htitle.c_str(), fNelem, 0, fNelem );
+  }
+#endif
+  return 0;
+}
+
+//_____________________________________________________________________________
+void Plane::Clear( Option_t* opt )
+{    
+  // Clear event-by-event data (hits)
+
+  fHits->Clear(opt);
+  fFitCoords->Clear(opt);
+}
+
+//_____________________________________________________________________________
+Int_t Plane::End( THaRunBase* /* run */ )
+{
+#ifdef TESTCODE
+  // Write diagnostic histograms to file
+
+  if( TestBit(kDoHistos) ) {
+    if( fHitMap ) fHitMap->Write();
+  }
+#endif
+  return 0;
+}
+
+//_____________________________________________________________________________
+Int_t Plane::ReadDatabase( const TDatime& date )
+{
+  // Read database
+
+  static const char* const here = "ReadDatabase";
+
+  FILE* file = OpenFile( date );
+  if( !file ) return kFileError;
+
+  // Set defaults
+  TString plane_type;
+  Int_t required = 0, do_histos = 0;
+  fMaxHits   = kMaxUInt;
+
+  // Read fOrigin (plane position) and fSize. fOrigin is the chamber
+  // position relative to the Tracker reference frame - which in turn can be
+  // offset as a whole. Thus, with respect to some absolute frame
+  // (whatever it may be), each Plane is positioned at 
+  // fOrigin(Tracker) + fOrigin(plane)
+  Int_t status = ReadGeometry( file, date, kTRUE );
+
+  if( status == kOK ) {
+    vector<Int_t>* detmap = 0;
+    try {
+      // Putting this container on the stack may cause a stack overflow
+      detmap = new vector<Int_t>;
+
+      const DBRequest request[] = {
+	{ "detmap",         detmap,           kIntV },
+	{ "xp.res",         &fResolution,     kDouble,  0, 0, -1 },
+	{ "maxhits",        &fMaxHits,        kUInt,    0, 1, -1 },
+	{ "required",       &required,        kInt,     0, 1 },
+	{ "type",           &plane_type,      kTString, 0, 1 },
+	{ "description",    &fTitle,          kTString, 0, 1 },
+	{ "partner",        &fPartnerName,    kTString, 0, 1 },
+	{ "do_histos",      &do_histos,       kInt,     0, 1, -1 },
+	{ 0 }
+      };
+
+      status = LoadDB( file, date, request, fPrefix );
+
+      if( status == kOK ) {
+	UInt_t flags = TestBit(kHaveRefChans) ? THaDetMap::kFillRefChan : 0;
+	// Parse the detector map of the data channels
+	if( FillDetMap( *detmap, flags, here ) == 0 )
+	  status = kInitError;
+      }
+      delete detmap;
+    }
+    // Catch exceptions here so that we can close the file and clean up
+    catch(...) {
+      delete detmap;
+      fclose(file);
+      throw;
+    }
+  }
+
+  // Finished reading the database
+  fclose(file);
+  if( status != kOK )
+    return status;
+
+  // Get the Tracker detector we're part of
+  Tracker* tracker = dynamic_cast<Tracker*>( GetMainDetector() );
+  assert(tracker);
+
+  // Retrieve DAQ module parameters for our crateslots
+  for( Int_t imod = 0; imod < fDetMap->GetSize(); ++imod ) {
+    THaDetMap::Module* d = fDetMap->GetModule(imod);
+    tracker->LoadDAQmodel(d);
+    tracker->LoadDAQresolution(d);
+    if( TestBit(kTDCReadout) )
+      d->MakeTDC();
+    else
+      d->MakeADC();
+    UInt_t nchan = tracker->GetDAQnchan(d);
+    if( d->hi >= nchan ) {
+      Error( Here(here), "Detector map channel out of range for module "
+          "cr/sl/lo/hi = %u/%u/%u/%u. Must be < %u. Fix database.",
+          d->crate, d->slot, d->lo, d->hi, nchan );
+      return kInitError;
+    }
+  }
+
+  // Sanity checks
+  if( fNelem <= 0 or fNelem > 1000000 ) { // arbitray upper limit
+    Error( Here(here), "Invalid number of channels: %d", fNelem );
+    return kInitError;
+  }
+
+  Int_t nchan = fDetMap->GetTotNumChan();
+  if( nchan != fNelem ) {
+    Error( Here(here), "Number of detector map channels (%d) "
+	   "disagrees with number of wires/strips (%d)", nchan, fNelem );
+    return kInitError;
+  }
+
+  // Determine the type of this plane. If the optional plane type variable is
+  // not given, use the first character of the plane name.
+  TString name = plane_type.IsNull() ? fName[0] : plane_type[0];
+  fType = Projection::NameToType( name );
+  if( fType == kUndefinedType ) {
+    TString names;
+    for( EProjType i = kTypeBegin; i < kTypeEnd; ++i ) {
+      names += kProjParam[i].name;
+      if( i+1 != kTypeEnd ) 
+        names += " ";
+    }
+    Error( Here(here), "Unsupported plane type \"%s\". Must be one of "
+        "%s. Fix database.", name.Data(), names.Data() );
+    return kInitError;
+  }
+
+  if( required )
+    SetBit( kIsRequired );
+
+#ifdef TESTCODE
+  if( do_histos )
+    SetBit( kDoHistos );
+#endif
+
+  // Update fCoordOffset based on the orientation of fProjection (if defined)
+  UpdateOffset();
+
+  fIsInit = true;
+  return kOK;
+}
+//_____________________________________________________________________________
+void Plane::SetPartner( Plane* p )
+{    
+  // Partner this plane with plane 'p'. The meaning of "partner" depends on the
+  // type of detector:
+  // For wire chambers:
+  //  -> a plane immediately next to this one with staggered wires
+  // For 2-D strip readouts (GEMs):
+  //  -> the plane that represents the other readout coordinate
+
+  if( p )
+    p->fPartner = this;
+  else if( fPartner ) {
+    assert( this == fPartner->fPartner );
+    fPartner->fPartner = 0;
+  }
+  fPartner = p;
+
+  return;
+}
+
+//_____________________________________________________________________________
+void Plane::UpdateOffset()
+{
+  // Update fCoordOffset based on the orientation of the associated projection
+  // and this plane's fOrigin.
+
+  if( fProjection ) {
+    // Calculate the offset to be applied to the sensor positions if the
+    // chamber has a position offset. This will translate the hit positions
+    // into the Tracker frame, which is the reference frame for tracking
+    TVector2 off( fOrigin.X(), fOrigin.Y() );
+    fCoordOffset = off * fProjection->GetAxis();
+  } else
+    fCoordOffset = 0;
+}
+
+//_____________________________________________________________________________
+void Plane::Print( Option_t* ) const
+{    
+  // Print plane info
+
+  cout << "Plane:  #" << GetPlaneNum() << " "
+    << GetName()   << "\t"
+    //       << GetTitle()        << "\t"
+    << fNelem << " channels\t"
+    << "z = " << GetZ();
+  if( fPartner ) {
+    cout << "\t partner = " 
+      << fPartner->GetName();
+    //	 << fPartner->GetTitle();
+  }
+  cout << endl;
+}
+
+//_____________________________________________________________________________
+// Int_t Plane::Compare( const TObject* obj ) const 
+// {
+//   // Used to sort planes in a TCollection/TList by z-position
+
+//   // Fail if comparing to some other type of object
+//   assert( obj && IsA() == obj->IsA() );
+
+//   if( obj == this )
+//     return 0;
+
+//   const Plane* other = static_cast<const Plane*>( obj );
+//   if( GetZ() < other->GetZ() ) return -1;
+//   if( GetZ() > other->GetZ() ) return  1;
+//   return 0;
+// }
+
+//_____________________________________________________________________________
+Double_t Plane::GetMaxSlope() const
+{ 
+  return fProjection ? fProjection->GetMaxSlope() : kBig;
+}
+
+//_____________________________________________________________________________
+
+}
+
+ClassImp(TreeSearch::Plane)
+
+  ///////////////////////////////////////////////////////////////////////////////
+
---
+++ Plane.h
@@ -0,0 +1,203 @@
+//*-- Author:  Ole Hansen<mailto:ole@jlab.org>; Jefferson Lab; 11-Jan-2010
+//
+#ifndef ROOT_TreeSearch_Plane
+#define ROOT_TreeSearch_Plane
+
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+// TreeSearch::Plane                                                         //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+#include "Types.h"
+#include "THaSubDetector.h"
+#include "TClonesArray.h"
+#include "TVector2.h"
+#include "TMath.h"
+#include <vector>
+#include <string>
+#include <cassert>
+#include <functional>
+
+using std::vector;
+using std::string;
+
+class TH1;
+
+namespace TreeSearch {
+
+  class Projection;
+  class Tracker;
+  class Hit;
+  class FitCoord;
+  extern const Double_t kBig;
+
+  class Plane : public THaSubDetector {
+
+  public:
+    Plane( const char* name, const char* description = "", 
+	   THaDetectorBase* parent = 0 );
+    // For ROOT RTTI
+    Plane() : fPartner(0), fProjection(0), fHits(0), fFitCoords(0),
+	      fHitMap(0) {}
+    virtual ~Plane();
+
+    virtual void    Clear( Option_t* opt="" );
+    virtual Int_t   Decode( const THaEvData& ) = 0;
+    virtual void    Print( Option_t* opt="" ) const;
+
+    virtual Int_t   Begin( THaRunBase* r=0 );
+    virtual Int_t   End( THaRunBase* r=0 );
+
+//     virtual Int_t   Compare ( const TObject* obj ) const;
+//     virtual Bool_t  IsSortable () const { return kTRUE; }
+
+    FitCoord*       AddFitCoord( const FitCoord& coord );
+    Bool_t          Contains( Double_t x, Double_t y ) const;
+    Bool_t          Contains( const TVector2& point ) const;
+    void            EnableCalibration( Bool_t enable = true );
+    EProjType       GetType()        const { return fType; }
+    Double_t        GetZ()           const { return fOrigin.Z(); }
+    Projection*     GetProjection()  const { return fProjection; }
+    Plane*          GetPartner()     const { return fPartner; }
+    void            SetPartner( Plane* p );
+    const char*     GetPartnerName() const { return fPartnerName.Data(); }
+    Tracker*        GetTracker()     const;
+    Double_t        GetResolution()  const { return fResolution; }
+    Double_t        GetMaxSlope()    const; 
+    Double_t        GetStart()       const { return fStart+fCoordOffset; }
+    Double_t        GetPitch()       const { return fPitch; }
+
+    TSeqCollection* GetHits()        const { return fHits; }
+    Int_t           GetNhits()       const { return fHits->GetLast()+1; }
+    TSeqCollection* GetCoords()      const { return fFitCoords; }
+    Int_t           GetNcoords()     const { return fFitCoords->GetLast()+1; }
+    UInt_t          GetPlaneNum()    const { return fPlaneNum; }
+    UInt_t          GetDefinedNum()  const { return fDefinedNum; }
+    Bool_t          IsCalibrating()  const { return TestBit(kCalibrating); }
+    Bool_t          IsRequired()     const;
+
+    void            SetPlaneNum( UInt_t n )   { fPlaneNum = n; }
+    void            SetDefinedNum( UInt_t n ) { fDefinedNum = n; }
+    void            SetProjection( Projection* p );
+    void            SetRequired( Bool_t enable = true );
+    void            UpdateOffset();
+
+    // Helper functors for STL algorithms...
+    struct ZIsLess
+      : public std::binary_function< Plane*, Plane*, bool >
+    {
+      bool operator() ( const Plane* a, const Plane* b ) const
+      { 
+	assert( a && b );
+	assert( a->GetDefinedNum() != b->GetDefinedNum() );
+	if( a->GetZ() < b->GetZ() ) return true;
+	if( b->GetZ() < a->GetZ() ) return false;
+	return( a->GetDefinedNum() < b->GetDefinedNum() );
+      }
+    };
+
+    class NameEquals : public std::unary_function< Plane*, bool > {
+    public:
+      NameEquals( const char* s ) : name(s?s:"") {}
+      bool operator() ( const Plane* pl ) const
+      { assert(pl); return ( name == pl->GetName() ); }
+    private:
+      string name;
+    };
+
+  protected:
+
+    // Geometry, configuration
+    UInt_t        fPlaneNum;    // Ordinal of this plane within its projection
+    UInt_t        fDefinedNum;  // Ordinal of this plane in planeconfig string
+    EProjType     fType;        // Plane type (e.g. x,y,u,v)
+    Double_t      fStart;       // Position of 1st sensor (e.g. wire/strip) (m)
+    Double_t      fPitch;       // Sensor pitch (assumed constant) (m)
+    Double_t      fCoordOffset; // Coord offset wrt Tracker due to fOrigin
+    Plane*        fPartner;     //! Partner plane
+    Projection*   fProjection;  //! The projection that we belong to
+    TString       fPartnerName; // Name of partner plane in same 2-D readout
+
+    // Parameters, calibration, flags
+    Double_t      fResolution;  // Sensor position resolution (sigma) (m)
+    UInt_t        fMaxHits;     // Maximum # hits before flagging decode error
+
+    // Event data, hits etc.
+    TClonesArray* fHits;        // Cluster data (groups of hits)
+    TClonesArray* fFitCoords;   // Cluster coordinates used by good road fits
+
+    // Podd interface
+    virtual Int_t ReadDatabase( const TDatime& date );
+
+    // Only for TESTCODE, but kept for binary compatibility
+    TH1*          fHitMap;     // Histogram of active sensor numbers
+
+    // Bits for Planes
+    enum {
+      kIsRequired  = BIT(14), // Tracks must have a hit in this plane
+      kCalibrating = BIT(15), // Plane in calibration mode (implies !required)
+      kTDCReadout  = BIT(21), // Readout is TDC-based
+      kHaveRefChans= BIT(22)  // Hardware modules have reference channels
+#ifdef TESTCODE
+     ,kDoHistos    = BIT(23)  // Generate diagnostic histograms
+#endif
+    };
+
+    ClassDef(Plane,0)  // One Tracker plane coordinate direction
+  };
+
+  //___________________________________________________________________________
+  inline
+  Bool_t Plane::Contains( Double_t x, Double_t y ) const
+  {
+    // Check if the given point is within the active area of this wire plane.
+    // Coordinates are relative to the Tracker origin. Time-critical, may be
+    // be called O(1e5) per event
+
+    //TODO: allow for (small) rotation due to misalignment?
+
+    return ( TMath::Abs( x-fOrigin.X() ) < fSize[0] and
+	     TMath::Abs( y-fOrigin.Y() ) < fSize[1] );
+  }
+
+  //___________________________________________________________________________
+  inline
+  Bool_t Plane::Contains( const TVector2& point ) const
+  {
+    // Same as Contains(x,y), but using a TVector2 as input
+
+    return Contains( point.X(), point.Y() );
+  }
+
+  //___________________________________________________________________________
+  inline
+  void Plane::EnableCalibration( Bool_t enable )
+  {
+    // Enable/disable calibration mode flag
+
+    SetBit( kCalibrating, enable );
+  }
+  
+  //___________________________________________________________________________
+  inline
+  Bool_t Plane::IsRequired() const
+  { 
+    return ( not IsCalibrating() and TestBit(kIsRequired) );
+  }
+
+  //___________________________________________________________________________
+  inline
+  void Plane::SetRequired( Bool_t enable )
+  {
+    // Enable/disable calibration mode flag
+
+    SetBit( kIsRequired, enable );
+  }
+  
+///////////////////////////////////////////////////////////////////////////////
+
+} // end namespace TreeSearch
+
+
+#endif
