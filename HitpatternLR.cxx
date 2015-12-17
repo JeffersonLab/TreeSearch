JeffersonLab/TreeSearch

commit 958349a8e3bb3098ccfdd5def88941bfa04576aa

Consistently set 2-sigma position ranges in Hitpatterns
We define the hit resolution as 1 sigma (as needed for the various
chi2 calculations), so we should cover more than 1 sigma when setting
the hitpattern position range. One might even consider 3 sigma, but
the argument is actually mostly academic since the hitpatterns are
usually binned much coarser than the hit resolution (say, 1mm
vs. 0.1mm) so that factors of 2 or 3 in the resolution only matter at
bin boundaries, and even if a neighboring bin is missed, the one
actually set frequently belongs to a valid pattern as well. Thus, 2 vs
3 sigma here most certainly is not equivalent to going from 95% to
99.8% hit detection efficiency.

HitpatternLR.cxx
@@ -153,7 +153,8 @@ Int_t HitpatternLR::ScanHits( Plane* A, Plane* B )
 	else      { posB = hitB->GetPosR(); bitB = 1; }
 	if( TMath::Abs( posA-posB ) <= maxdist ) {
 	  if( (bitA & set) == 0 ) {
-	    SetPosition( posA+fOffset, hitA->GetResolution(), planeA, hitA );
+	    // Cover +/- 2 sigma position range
+	    SetPosition( posA+fOffset, 2.*hitA->GetResolution(), planeA, hitA );
 	    // Prevent duplicate entries for zero-drift hits
 	    if( hitA->GetDriftDist() == 0 )
 	      set |= 12;
@@ -161,7 +162,7 @@ Int_t HitpatternLR::ScanHits( Plane* A, Plane* B )
 	      set |= bitA;
 	  }
 	  if( (bitB & set) == 0 ) {
-	    SetPosition( posB+fOffset, hitB->GetResolution(), planeB, hitB );
+	    SetPosition( posB+fOffset, 2.*hitB->GetResolution(), planeB, hitB );
 	    if( hitB->GetDriftDist() == 0 )
 	      set |= 3;
 	    else
@@ -173,14 +174,14 @@ Int_t HitpatternLR::ScanHits( Plane* A, Plane* B )
     else if( do_single_hits ) {
       // Unpaired hit in only one plane
       if( hitA ) {
-	SetPosition( hitA->GetPosL()+fOffset, hitA->GetResolution(),
+	SetPosition( hitA->GetPosL()+fOffset, 2.*hitA->GetResolution(),
 		     planeA, hitA );
-	SetPosition( hitA->GetPosR()+fOffset, hitA->GetResolution(),
+	SetPosition( hitA->GetPosR()+fOffset, 2.*hitA->GetResolution(),
 		     planeA, hitA );
       } else {
-	SetPosition( hitB->GetPosL()+fOffset, hitB->GetResolution(),
+	SetPosition( hitB->GetPosL()+fOffset, 2.*hitB->GetResolution(),
 		     planeB, hitB );
-	SetPosition( hitB->GetPosR()+fOffset, hitB->GetResolution(),
+	SetPosition( hitB->GetPosR()+fOffset, 2.*hitB->GetResolution(),
 		     planeB, hitB );
       }
     }

Hitpattern.cxx
@@ -284,6 +284,7 @@ Int_t Hitpattern::ScanHits( Plane* pl, Plane* )
 #endif
   while( (phit = static_cast<Hit*>(it->Next())) ) {
     ++nhits;
+    // Cover +/- 2 sigma position range
     SetPosition( phit->GetPos()+fOffset, 2.0*phit->GetResolution(),
 		 plane, phit );
 #ifndef NDEBUG

commit 51d2829769fc4d2d89b3d4b68fbfe1481d11f312

Fix regression in HitpattternLR: wrong # of sigmas in SetPosition calls
The factor of 2 introduced in commit 958349a (Nov 1, 2014) is now
taken care of by the parameter kNResSig introduced in commit 3d6ba6f
(Jan 31, 2015). In that latter commit, Hitpattern::ScanHits was changed,
but the corresponding change in HitpatternLR::ScanHits was apparently
forgotten.

HitpatternLR.cxx
@@ -157,7 +157,7 @@ Int_t HitpatternLR::ScanHits( Plane* A, Plane* B )
 	if( TMath::Abs( posA-posB ) <= maxdist ) {
 	  if( (bitA & set) == 0 ) {
 	    // Cover +/- 2 sigma position range
-	    SetPosition( posA+fOffset, 2.*hitA->GetResolution(), planeA, recA );
+	    SetPosition( posA+fOffset, hitA->GetResolution(), planeA, recA );
 	    // Prevent duplicate entries for zero-drift hits
 	    if( hitA->GetDriftDist() == 0 )
 	      set |= 12;
@@ -165,7 +165,7 @@ Int_t HitpatternLR::ScanHits( Plane* A, Plane* B )
 	      set |= bitA;
 	  }
 	  if( (bitB & set) == 0 ) {
-	    SetPosition( posB+fOffset, 2.*hitB->GetResolution(), planeB, recB );
+	    SetPosition( posB+fOffset, hitB->GetResolution(), planeB, recB );
 	    if( hitB->GetDriftDist() == 0 )
 	      set |= 3;
 	    else
@@ -177,14 +177,14 @@ Int_t HitpatternLR::ScanHits( Plane* A, Plane* B )
     else if( do_single_hits ) {
       // Unpaired hit in only one plane
       if( hitA ) {
-	SetPosition( hitA->GetPosL()+fOffset, 2.*hitA->GetResolution(),
+	SetPosition( hitA->GetPosL()+fOffset, hitA->GetResolution(),
 		     planeA, recA );
-	SetPosition( hitA->GetPosR()+fOffset, 2.*hitA->GetResolution(),
+	SetPosition( hitA->GetPosR()+fOffset, hitA->GetResolution(),
 		     planeA, recA );
       } else {
-	SetPosition( hitB->GetPosL()+fOffset, 2.*hitB->GetResolution(),
+	SetPosition( hitB->GetPosL()+fOffset, hitB->GetResolution(),
 		     planeB, recB );
-	SetPosition( hitB->GetPosR()+fOffset, 2.*hitB->GetResolution(),
+	SetPosition( hitB->GetPosR()+fOffset, hitB->GetResolution(),
 		     planeB, recB );
       }
     }